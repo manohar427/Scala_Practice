7466581909a3f829a31b6435d0847f97212490ae
unknown <manohar427@gmail.com>
2017-01-22 21:09:47 +0530

    Scala 6

--- Scala_6/src/ListDemo.scala
+++ Scala_6/src/ListDemo.scala
@@ -4,7 +4,7 @@ object ListDemo {
   def main(arg: Array[String]) {
     var listSample: List[String] = List("One", "Two");
     var setSample: Set[String] = Set("A", "B");
-
+    var s: Set[Int] = Set(1, 3, 5, 7)
     for (i <- listSample) {
       println(i);
     }
