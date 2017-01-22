7bbaf510e0e8af22cab50d6902142f58fc68f8b1
unknown <manohar427@gmail.com>
2017-01-22 20:30:55 +0530

    Scala 2

---
+++ Scala_2/src/Test.scala
@@ -0,0 +1,15 @@
+
+
+object Test {
+  def main(args: Array[String]) {
+    var x: Int = 90;
+    var fl: Float = 133.44f;
+    var dob: Double = 90;
+    var str: String = "Manohar Sambayyapalem";
+    
+    println("x:" + x);
+    println("fl:" + fl);
+    println("dob:" + dob);
+    println("str:" + str);
+  }
+}
